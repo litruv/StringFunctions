178edc391b42705cdb692ad2fb512b7b95bf2132
Max Litruv Boonzaayer <litruv@gmail.com>
2022-12-13 11:43:47 +1100

    added ts definition

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
 export declare const splitParagraph: (paragraph: string, maxLength: number) => string[];
 export declare const splitString: (str: string, maxLength: number) => string[];
+export declare function containsURL(str: string) => boolean;
